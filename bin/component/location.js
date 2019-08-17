274d33a494fd3f374009dfc2839467f099ffc531
zouzenghu <zouzenghu@163.com>
2019-08-17 22:34:13 +0800

    添加npm-typescript模板

--- bin/component/location.js
+++ bin/component/location.js
@@ -3,5 +3,10 @@ module.exports = {
     url: "https://github.com/zouzenghu/ES6-template",
     downloadUrl: "https://github.com:zouzenghu/ES6-template#master",
     description: "webpack——ES6配置"
+  },
+  npmTypescript: {
+    url: "https://github.com/zouzenghu/npm-typescript-template",
+    downloadUrl: "https://github.com:zouzenghu/npm-typescript-template#master",
+    description: "npm-typescript包基本配置"
   }
 };
