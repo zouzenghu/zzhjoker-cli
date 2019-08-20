d702bd18179d159ccf9205799bd116551c1b0607
zouzenghu <zouzenghu@163.com>
2019-08-20 19:00:19 +0800

    add WebpackLibrary

--- bin/component/location.js
+++ bin/component/location.js
@@ -1,12 +1,17 @@
 module.exports = {
   webpack: {
-    url: "https://github.com/zouzenghu/npm-webpack-es6-template",
-    downloadUrl: "https://github.com:zouzenghu/npm-webpack-es6-template#master",
-    description: "webpack——ES6配置"
+    url: 'https://github.com/zouzenghu/npm-webpack-es6-template',
+    downloadUrl: 'https://github.com:zouzenghu/npm-webpack-es6-template#master',
+    description: 'webpack——ES6配置'
   },
   TypescriptPackage: {
-    url: "https://github.com/zouzenghu/npm-typescript-template",
-    downloadUrl: "https://github.com:zouzenghu/npm-typescript-template#master",
-    description: "npm-typescript包配置"
+    url: 'https://github.com/zouzenghu/npm-typescript-template',
+    downloadUrl: 'https://github.com:zouzenghu/npm-typescript-template#master',
+    description: 'npm-typescript包配置'
+  },
+  WebpackLibrary: {
+    url: 'https://github.com/zouzenghu/npm-typescript-template',
+    downloadUrl: 'https://github.com:zouzenghu/npm-typescript-template#master',
+    description: 'WebpackLibrary包配置'
   }
-};
+}
